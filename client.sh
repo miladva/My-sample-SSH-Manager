810fa6ab9e11740a15b87d9e7a9387d9a3a776cd
Milad vayani <76900115+miladvayani@users.noreply.github.com>
2023-03-09 14:48:30 +0330

    Update client.sh

--- client.sh
+++ client.sh
@@ -4,7 +4,7 @@
 useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "$2") $1
 chown -R root:root /home/$1/
 usermod -a -G vpn $1
-usermod -e `date --date='+2 month' '+%Y-%m-%d'` $1
+usermod -e `date --date='+$3 days' '+%Y-%m-%d'` $1
 
 echo $1 >> /root/users.txt
 
