7544b24551c107f4a3d221822a67e0ab5c4b9cfa
Milad <76900115+miladva@users.noreply.github.com>
2023-05-12 21:30:29 +0330

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,7 +4,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
 while read line; do
-  if [[ `ps -u $line | grep sshd | wc -l` -ge 15 ]]
+  if [[ `ps -u $line | grep sshd | wc -l` -ge 5 ]]
   then
     PROCESSES=`ps -u $line | grep sshd | awk '{ print $1 }'`
     sudo killall -u $line
@@ -13,11 +13,11 @@ while read line; do
     echo "found user $line with process count: `ps -u $line | grep sshd | wc -l` -- pids: $PROCESSES" >> /root/events.txt
   fi
 
-  if [[ `ps -u $line | grep sshd | wc -l` -ge 3 ]]
-  then
-    PROCESSES=`ps -u $line | grep sshd | awk '{ print $1 }'`
-    echo "found user $line with process count: `ps -u $line | grep sshd | wc -l` -- pids: $PROCESSES" >> /root/dis.txt 
-    kill -9 $PROCESSES
-  fi
+  #if [[ `ps -u $line | grep sshd | wc -l` -ge 3 ]]
+  #then
+   # PROCESSES=`ps -u $line | grep sshd | awk '{ print $1 }'`
+    #echo "found user $line with process count: `ps -u $line | grep sshd | wc -l` -- pids: $PROCESSES" >> /root/dis.txt 
+    #kill -9 $PROCESSES
+ #fi
 
 done < /root/users.txt
