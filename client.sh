0df0c923470f52a4aa2bb327416e501376318d8c
Milad <76900115+miladva@users.noreply.github.com>
2023-06-13 18:09:31 +0330

    Update client.sh

--- client.sh
+++ client.sh
@@ -4,8 +4,14 @@
 useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "$2") $1
 chown -R root:root /home/$1/
 usermod -a -G vpn $1
-usermod -e `date --date='+2 month' '+%Y-%m-%d'` $1
 
+days = 31
+
+# Calculate the new date
+new_date=$(date --date="+${days} days" "+%Y-%m-%d")
+
+# Update user account with new date
+usermod -e "$new_date" "$1"
 echo $1 >> /root/users.txt
 echo $1 $2 >> /root/userpass.txt
 echo $1 $2
