41f046fb3a319f0137f658a44e0b092dc8fe053d
root <root@busy-behavior.aeza.network>
2023-07-05 12:35:28 +0300

    add auto password generator for backup.sh

--- backup.sh
+++ backup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 #
+password=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 5 | head -n 1)
 
-useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "$2") $1
+file_path="/root/userpass.txt"
+new_date=$(date --date="+${days} days" "+%Y-%m-%d")
+
+
+if id "$1" >/dev/null 2>&1; then
+    usermod -p $(perl -e 'print crypt($ARGV[0], "password")' "$password") $1
+    sed -i "/$1/c\\$1 $password $2" "$file_path"
+else
+    useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "$password") $1
+    echo $1 >> /root/users.txt
+    echo $1 $password $2 >> /root/userpass.txt
+
+fi
+
 chown -R root:root /home/$1/
 usermod -a -G vpn $1
 
-days=$3
-
-# Calculate the new date
-new_date=$(date --date="+${days} days" "+%Y-%m-%d")
+days=$2
 
 # Update user account with new date
 usermod -e "$new_date" "$1"
-echo $1 >> /root/users.txt
-echo $1 $2 >> /root/userpass.txt
-echo $1 $2 $3
+echo $1 $password $2
