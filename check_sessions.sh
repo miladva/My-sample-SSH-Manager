c8d98eb685edcd7e64157737b2e6b3d4c95bebeb
Milad <76900115+miladva@users.noreply.github.com>
2023-12-19 01:38:46 +0330

    Update check_sessions.sh

--- check_sessions.sh
+++ check_sessions.sh
@@ -1,15 +1,8 @@
-#!/bin/bash
-if [ -z "$SSH_ORIGINAL_COMMAND" ]; then
-    # This is an interactive session
-    MAX_SESSIONS=14
-    CURRENT_SESSIONS=$(ps aux | grep sshd: | grep -v grep | wc -l)
-    if [ "$CURRENT_SESSIONS" -lt "$MAX_SESSIONS" ]; then
-        exec /bin/bash
-    else
-        echo "Maximum number of SSH sessions reached."
-        exit 1
-    fi
+MAX_SESSIONS=13
+CURRENT_SESSIONS=$(ps aux | grep sshd: | grep -v grep | wc -l)
+if [ "$CURRENT_SESSIONS" -lt "$MAX_SESSIONS" ]; then
+    exec /bin/bash
 else
-    # This is a non-interactive session
-    exec $SSH_ORIGINAL_COMMAND
+    echo "Maximum number of SSH sessions reached."
+    exit 1
 fi
