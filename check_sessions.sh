3f81a2bb61c6ad75b7afae64c2360a7d00c26b1a
Milad <76900115+miladva@users.noreply.github.com>
2023-12-19 01:28:05 +0330

    Create check_sessions.sh

---
+++ check_sessions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MAX_SESSIONS=10
+CURRENT_SESSIONS=$(ps aux | grep sshd: | grep -v grep | wc -l)
+if [ "$CURRENT_SESSIONS" -lt "$MAX_SESSIONS" ]; then
+    exec /bin/bash
+else
+    echo "Maximum number of SSH sessions reached."
+    exit 1
