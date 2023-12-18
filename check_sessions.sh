a6f5264aaf6b627781fe1f7c58cdd002e30a26b4
Milad <76900115+miladva@users.noreply.github.com>
2023-12-19 01:46:34 +0330

    Update check_sessions.sh

--- check_sessions.sh
+++ check_sessions.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
+
+# Maximum allowed SSH sessions
 MAX_SESSIONS=6
+
+# Count the current number of SSH child processes
 CURRENT_SESSIONS=$(ps aux | grep sshd: | grep -v grep | wc -l)
-if [ "$CURRENT_SESSIONS" -lt "$MAX_SESSIONS" ]; then
-    exec /bin/bash
+
+# Check if the current number of sessions is less than the maximum
+if [ "$CURRENT_SESSIONS" -lt "$MAX_SESSIONS" ] || [ -n "$SSH_ORIGINAL_COMMAND" ]; then
+    # If below the limit, or if this is a non-interactive session, proceed
+    if [ -n "$SSH_ORIGINAL_COMMAND" ]; then
+        exec "$SSH_ORIGINAL_COMMAND"
+    else
+        exec "$SHELL"
+    fi
 else
-    echo "Maximum number of SSH sessions reached."
+    # If the limit is reached, deny the session and exit
+    echo "Maximum number of SSH sessions ($MAX_SESSIONS) reached. Try again later."
     exit 1
 fi
